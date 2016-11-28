a4e1d732da5381cdf9d329cd20415a1c588224ab
William Holt <wholt@odecee.com.au>
2016-11-28 15:10:48 +1100

    add wifi selector and clean code

--- arduino-climate.ino
+++ arduino-climate.ino
@@ -1,7 +1,20 @@
 #include <ESP8266WiFi.h>
 
-char ssid[] = "Holt-2.4Ghz";
-char pass[] = "67351932";
+// WiFi setup - set wifiIndex to the index of the credentials you want
+int wifiIndex = 1;
+char wifi[][2][64] = {
+  { "Holt-2.4Ghz", "67351932" },
+  { "COG_MEL_MAC_WIFI", "Ct$Mel@345" }
+};
+
+// Request destination settings
+const char* host = "lynq.me";
+const int port = 8000;
+String url = "/";
+
+String request = String("GET ") + url + " HTTP/1.1\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "Connection: close\r\n\r\n";
 
 void setup() {
   Serial.begin(115200);
@@ -10,8 +23,8 @@ void setup() {
 	pinMode(BUILTIN_LED, OUTPUT);
 
 	// Connect to WiFi
-	Serial.print("Connecting to WiFi");
-	WiFi.begin(ssid, pass);
+	Serial.print("\nConnecting to WiFi");
+	WiFi.begin(wifi[wifiIndex][0], wifi[wifiIndex][1]);
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(1000);
 		Serial.print(".");
@@ -21,9 +34,7 @@ void setup() {
 
 	// Connect to server
 	WiFiClient client;
-	const char* host = "192.168.0.2";
-	const int port = 8000;
-	Serial.print("Connecting to server ");
+	Serial.print("Connecting to server ... ");
 	if (!client.connect(host, port)) {
 		Serial.println("Connection failed");
 		return;
@@ -31,22 +42,24 @@ void setup() {
 	Serial.println("Connection succeeded");
 
   // Make request
-	String url = "/";
 	Serial.print("Requesting URL: "); Serial.println(url);
-	String request = String("GET ") + url + " HTTP/1.1\r\n" +
-											"Host: " + host + "\r\n" +
-											"Connection: close\r\n\r\n";
 	client.print(request);
-	Serial.println("Request: " + request);
+	Serial.println("Request:\n```\n" + request + "\n```");
 
-	delay(10);
+	delay(1000);   // This is the amount of time to give the request to complete
+                 // 50 is plenty for LAN under most conditions
+                 // For internet it varies a lot - 1000 should be pretty safe
+                 // unless your internet connection is bad
 
   // Receive response
-	Serial.println("Response: ");
+	Serial.println("Response:\n```\n");
 	while (client.available()) {
 		String line = client.readStringUntil('\r');
 		Serial.println(line);
 	}
+  Serial.println("\n```");
+
+  Serial.println("== DONE ==");
 }
 
 void loop() {
